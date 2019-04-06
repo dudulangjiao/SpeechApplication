ab214aaa8fdabe3fcdf7b69b8d31164e00f54b79
LxpHomePc <lin_bo_lin@126.com>
2019-04-07 00:56:14 +0800

    20190406 完成搜索关键词的分词，并查询文章列表

--- src/application/views.py
+++ src/application/views.py
@@ -1,8 +1,8 @@
 
 
-from flask import render_template, request
+from flask import render_template, request, g
 from src import app
-from src.application.database import db_session, Word_sheet, Speech_sheet
+from src.application.database import db_session, Word_sheet, Speech_sheet, Speaker_sheet
 from src.application.base import LtpProcess
 
 @app.route('/')
@@ -20,14 +20,17 @@ def submit_cm():
     if not key_words:
         return render_template('index.html', prompt='关键词不能为空，请重新输入：')
     else:
-        # 用类LtpProcess进行分词处理
+        # 对关键词用类LtpProcess进行分词处理
         ltp_instance = LtpProcess(area_unicode)
         word_list = ltp_instance.ltp_word()  # word_list类型是列表
         session = db_session()
+
+        # 对每一个分词逐一进行循环查询，找出出现所有分词的文章
         i = 0
         query_row_list = []
         for word_str in word_list:
             if i == 0:
+                #找出存在第一个分词的文章
                 query_row = session.query(Word_sheet.speech_id_of_word).filter_by(word_content=word_str)\
                                    .group_by(Word_sheet.speech_id_of_word).all()
                 print(type(query_row))
@@ -39,23 +42,32 @@ def submit_cm():
                 print(query_row_list)
 
             else:
+                #在存在第一个分词的文章中，继续找出是否也存在第二个、第三个.....分词
                 query_row = session.query(Word_sheet.speech_id_of_word)\
                     .filter(Word_sheet.speech_id_of_word.in_(query_row_list), Word_sheet.word_content==word_str) \
                     .group_by(Word_sheet.speech_id_of_word).all()
-
+            #把二维列表转化为一维列表
             query_row_list = []
             for query_row_str in query_row:
                 query_row_list.append(query_row_str.speech_id_of_word)
 
             i = i + 1
-
-        query_row_result = session.query(Speech_sheet.speech_id, Speech_sheet.speech_title) \
+        # 列出出现所有分词的文章
+        query_row_result = session.query(Speech_sheet.speech_id, Speaker_sheet.speaker_name, Speech_sheet.speech_title).join(Speaker_sheet) \
             .filter(Speech_sheet.speech_id.in_(query_row_list)).all()
 
         db_session.remove()
 
         return render_template('query_result_wb.html', query_outcome=query_row_result)
 
+
+@app.route('/b/<int:url_speech_id>', methods=['GET'])
+def content_cm(url_speech_id):
+    session = db_session()
+    sp_content = session.query(Speech_sheet.speech_title, Speech_sheet.speech_content).filter(Speech_sheet.speech_id==url_speech_id).all()
+    title_content = sp_content[0][0]
+    sp_content = sp_content[0][1]
+    return render_template('speech_context.html', content_title = title_content, content_state=sp_content)
 """
 @app.route('/b', methods=['GET'])
 def get_post():
