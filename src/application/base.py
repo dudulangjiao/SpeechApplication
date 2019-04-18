7f8a9a831597c27a145b8ef5e2e4a160dce796a4
LxpHomePc <lin_bo_lin@126.com>
2019-04-18 20:16:33 +0800

    20190418

--- src/application/base.py
+++ src/application/base.py
@@ -32,7 +32,8 @@ class StringSortWeight(object):
     """函数:给定一个词组列表和一个字符串，查找字符串中是否含有词组列表的任何组合，并返回权重.
 
     Attributes:
-                content: 要处理的文本内容
+                word_list: 词组列表
+                str_content： 字符串内容
 
     """
     def __init__(self, word_list, str_content):
@@ -49,17 +50,17 @@ class StringSortWeight(object):
 
     def weight(self):
         """创建一个方法，给定word_list中要连接字符串的起始位置和连接的次数，返回连接的字符串"""
-        word_list_number = (len(self.word_list))
+        word_list_number = (len(self.word_list))  #1
         sort_weight = 0  # 权重
         for word_int in range(word_list_number):
-            word_long = word_list_number - word_int  # 从最长的词组组合长度word_list_number开始
-            combination_number = word_int + 1  #  各种组合的可能性数量
+            word_long = word_list_number - word_int  # 从最长的词组组合长度word_list_number开始 1
+            combination_number = word_int + 1  #  各种组合的可能性数量 1
             # 循环取得给定长度的所有词组组合,以词组组合在字符串中出现的次数，赋予其权重并对权重进行累加，返回最后总权重
-            for begin_str in range(combination_number):
+            for begin_str in range(combination_number): #1
                 tmp_word_str = self.word_join(begin_str, word_long)
                 integration = self.str_content.count(tmp_word_str)
                 if integration > 0:  # 若词组组合在字符串中出现的次数大于0
-                    sort_weight = pow(integration, 5) + 10*integration + sort_weight
+                    sort_weight = integration*pow(word_long, word_long) + sort_weight
 
         return sort_weight
 
